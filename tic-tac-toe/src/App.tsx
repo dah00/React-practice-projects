628177c7b59f9ded7a7368b64ee5c83d9e28abda
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-08-10 16:34:03 -0700

    Added draw function.

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -12,10 +12,11 @@ function App() {
    * [0,0] [0,1] [0,2]
    * [1,0] [1,1] [1,2]
    * [2,0] [2,1] [2,2]
-   * 
+   *
    */
 
-  const [winner, setWinner] = useState(false);
+  const [gameFinished, SetGameFinished] = useState(false);
+  const [winner, setWinner] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [turn, setTurn] = useState(true);
   // true - X (O player had the last turn)
@@ -23,51 +24,82 @@ function App() {
 
   function handleClick(x: number, y: number) {
     setTable((prevState) => {
-      const newState = [...prevState]
-      if(turn && newState[x][y] === 'null')
-        newState[x][y] = 'X'
-      else if(!turn && newState[x][y] === 'null')
-        newState[x][y] = 'O'
-      return newState;        
+      const newState = [...prevState];
+      if (turn && newState[x][y] === 'null') newState[x][y] = 'X';
+      else if (!turn && newState[x][y] === 'null') newState[x][y] = 'O';
+      return newState;
     });
     setTurn(!turn);
   }
 
   useEffect(() => {
     // horizontal checks
-    for(let i=0; i<3; i++){
-      if(table[i][0] !== 'null' && 
-        table[i][0] === table[i][1] && table[i][1] === table[i][2]){
-          setWinner(true);
-        }
+    for (let i = 0; i < 3; i++) {
+      if (
+        table[i][0] !== 'null' &&
+        table[i][0] === table[i][1] &&
+        table[i][1] === table[i][2]
+      ) {
+        SetGameFinished(true);
+        setWinner(turn ? 'O' : 'X');
+        return;
+      }
     }
 
     // vertical checks
-    for(let i=0; i<3; i++){
-      if(table[0][i] !== 'null' && 
-        table[0][i] === table[1][i] && table[1][i] === table[2][i]){
-          setWinner(true);
-        }
+    for (let i = 0; i < 3; i++) {
+      if (
+        table[0][i] !== 'null' &&
+        table[0][i] === table[1][i] &&
+        table[1][i] === table[2][i]
+      ) {
+        SetGameFinished(true);
+        setWinner(turn ? 'O' : 'X');
+        return;
+      }
     }
 
-    // diagonal checks 
+    // diagonal checks
     // left to right diagonal
-    if(table[0][0] !== 'null' && 
-      table[0][0] === table[1][1] && table[1][1] === table[2][2]){
-        setWinner(true);
+    if (
+      table[0][0] !== 'null' &&
+      table[0][0] === table[1][1] &&
+      table[1][1] === table[2][2]
+    ) {
+      SetGameFinished(true);
+      setWinner(turn ? 'O' : 'X');
+      return;
     }
-    // right to left diagonal 
-    if(table[0][2] !== 'null' && 
-      table[0][2] === table[1][1] && table[1][1] === table[2][0]){
-        setWinner(true);
+    // right to left diagonal
+    if (
+      table[0][2] !== 'null' &&
+      table[0][2] === table[1][1] &&
+      table[1][1] === table[2][0]
+    ) {
+      SetGameFinished(true);
+      setWinner(turn ? 'O' : 'X');
+      return;
     }
-  }, [table])
+    // check if draw
+    let finished = true;
+    for (let i = 0; i < table.length; i++) {
+      for (let j = 0; j < table[i].length; j++) {
+        if (table[i][j] === 'null') {
+          finished = false;
+        }
+      }
+    }
+    if (finished) {
+      SetGameFinished(true);
+      setWinner('Draw');
+    }
+  }, [table]);
 
-  useEffect(() =>{
-    if(winner){
-      setModalIsOpen(true)
+  useEffect(() => {
+    if (gameFinished) {
+      setModalIsOpen(true);
     }
-  }, [winner, turn]);
+  }, [turn, gameFinished]);
 
   const closeModal = () => {
     setModalIsOpen(false);
@@ -77,16 +109,21 @@ function App() {
         ['null', 'null', 'null'],
         ['null', 'null', 'null'],
         ['null', 'null', 'null'],
-      ]
+      ];
       return newState;
-    })
-    setWinner(false);
-  }
+    });
+    SetGameFinished(false);
+  };
 
   return (
     <div className="app">
-      <Modal className='modal' isOpen={modalIsOpen} onRequestClose={closeModal} ariaHideApp={false}>
-        <p> Congratulations {turn ? "O won" : "X won"}</p>
+      <Modal
+        className="modal"
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        ariaHideApp={false}
+      >
+        <p> Congratulations {winner}</p>
         <button onClick={closeModal}>Replay</button>
       </Modal>
       <div className="table">
@@ -102,20 +139,26 @@ function App() {
           </div>
         </div>
         <div className="row middle">
-          <div className="block"onClick={() => handleClick(1, 0)}>
-            {table[1][0] !== 'null' && table[1][0]}</div>
+          <div className="block" onClick={() => handleClick(1, 0)}>
+            {table[1][0] !== 'null' && table[1][0]}
+          </div>
           <div className="block column2" onClick={() => handleClick(1, 1)}>
-            {table[1][1] !== 'null' && table[1][1]}</div>
+            {table[1][1] !== 'null' && table[1][1]}
+          </div>
           <div className="block" onClick={() => handleClick(1, 2)}>
-            {table[1][2] !== 'null' && table[1][2]}</div>
+            {table[1][2] !== 'null' && table[1][2]}
+          </div>
         </div>
         <div className="row">
           <div className="block" onClick={() => handleClick(2, 0)}>
-            {table[2][0] !== 'null' && table[2][0]}</div>
+            {table[2][0] !== 'null' && table[2][0]}
+          </div>
           <div className="block column2" onClick={() => handleClick(2, 1)}>
-            {table[2][1] !== 'null' && table[2][1]}</div>
+            {table[2][1] !== 'null' && table[2][1]}
+          </div>
           <div className="block" onClick={() => handleClick(2, 2)}>
-            {table[2][2] !== 'null' && table[2][2]}</div>
+            {table[2][2] !== 'null' && table[2][2]}
+          </div>
         </div>
       </div>
     </div>
