3e0fc1bf58e8246a097b2e9561370ccc1f22a41d
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2023-08-24 20:04:35 -0700

    Add a search function

--- movie-trailers/src/App.tsx
+++ movie-trailers/src/App.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import MovieCard from './components/MovieCard';
 
 export type Movie = {
@@ -15,33 +15,43 @@ function App() {
 
   const [movies, setMovies] = useState<Movie[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchKey, setSearchKey] = useState("");
 
   const API_URL = "https://api.themoviedb.org/3"
   const API_key = "5f81fc7e65df399d311583079038272d"
 
-  async function getMovies(page: number){
+  async function getMovies(page?: number, searchKey?: string){
+    const type = searchKey ? 'search' : 'discover'
+
     try{
-      const {data: {results}} = await axios.get(`${API_URL}/discover/movie/`,{
+      const {data: {results}} = await axios.get(`${API_URL}/${type}/movie`,{
         params:{
           api_key: API_key,
-          page: page
+          page: page,
+          query: searchKey
         }
       });
       console.log(results);
+      console.log(`SeachKey: ${searchKey}`);
       setMovies(results);
     }catch(error){
       console.error(`ERROR ${error}`);
     }
   }
 
+  const searchMovies = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await getMovies(currentPage, searchKey);
+  }
+
   useEffect(() => {
-    getMovies(currentPage);
+    getMovies(currentPage, searchKey);
   }, [currentPage])
 
   const renderMovies = () => {
     if(movies){
       return (
-        <div className='grid grid-cols-6 gap-4 justify-center bg-inherit'>
+        <div className='grid lg:grid-cols-6 gap-4 justify-center bg-inherit sm:grid-cols-4'>
           {movies.map(movie => (
             <MovieCard
               key={movie.id}
@@ -63,8 +73,15 @@ function App() {
   }
 
   return (
-    <div className="App flex flex-col justify-center items-center mx-auto w-90">
-      <h1 className='text-4xl font-bold'>Movie browser</h1>
+    <div className="App flex flex-col justify-center items-center mx-auto w-90 gap-y-5">
+      <header className='flex flex-col gap-y-5'>
+        <h1 className='text-4xl font-bold'>Movie browser</h1>
+        <form onSubmit={searchMovies}>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)} className='text-black'/>
+          <button type='submit'>Search</button>
+        </form>
+      </header>
+      {searchKey}
       <main>
         {renderMovies()}
       </main>
